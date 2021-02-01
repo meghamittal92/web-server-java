c86a0f179d8b209aa1da20f25d89af671bc90ab8
Megha Mittal <megha.mittal92@live.com>
2021-02-01 14:47:07 +0530

    Initial commit

---
+++ src/main/java/com/client/calorieserver/CalorieserverApplication.java
@@ -0,0 +1,13 @@
+package com.client.calorieserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieserverApplication.class, args);
+	}
+
+}
