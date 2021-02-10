28a8f459d6ef219e0427fd26d1a21069444f38d4
Megha Mittal <megha.mittal92@live.com>
2021-02-11 02:16:40 +0530

    Move Enable JPA configuration to AppConfig

--- src/main/java/com/client/calorieserver/CalorieserverApplication.java
+++ src/main/java/com/client/calorieserver/CalorieserverApplication.java
@@ -4,14 +4,18 @@ import com.client.calorieserver.repository.CustomRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(repositoryBaseClass = CustomRepositoryImpl.class)
+@EnableAutoConfiguration
+@Import({ AppConfig.class })
 public class CalorieserverApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CalorieserverApplication.class, args);
 	}
 
