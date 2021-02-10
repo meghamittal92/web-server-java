9a3769ae6680f9c24a0f8d91dfd385a204d12db3
Megha Mittal <megha.mittal92@live.com>
2021-02-11 00:33:42 +0530

    Add entity manager refresh for Calorie updates

--- src/main/java/com/client/calorieserver/CalorieserverApplication.java
+++ src/main/java/com/client/calorieserver/CalorieserverApplication.java
@@ -1,11 +1,14 @@
 package com.client.calorieserver;
 
+import com.client.calorieserver.repository.CustomRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomRepositoryImpl.class)
 public class CalorieserverApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/client/calorieserver/repository/CustomRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.client.calorieserver.repository;
+
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+
+import javax.persistence.EntityManager;
+
+public class CustomRepositoryImpl<T, ID>
+        extends SimpleJpaRepository<T, ID> implements CustomRepository<T, ID> {
+
+    private final EntityManager entityManager;
+
+    public CustomRepositoryImpl(JpaEntityInformation entityInformation,
+                                EntityManager entityManager) {
+        super(entityInformation, entityManager);
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void flushAndRefresh(T t) {
+        entityManager.flush();
+        entityManager.refresh(t);
+    }
+}
