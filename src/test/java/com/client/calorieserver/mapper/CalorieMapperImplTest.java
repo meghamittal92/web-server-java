7bf005640ed5ec80d9a2182dff7c6676bff1a9fc
Megha Mittal <megha.mittal92@live.com>
2021-02-12 11:10:25 +0530

    calorieMapper abstract test added

--- src/test/java/com/client/calorieserver/mapper/CalorieMapperImplTest.java
+++ src/test/java/com/client/calorieserver/mapper/CalorieMapperImplTest.java
@@ -1,11 +1,17 @@
 package com.client.calorieserver.mapper;
 
+import com.client.calorieserver.accessor.CalorieAccessor;
 import com.client.calorieserver.domain.dto.db.CalorieDTO;
-import com.client.calorieserver.domain.mapper.CalorieMapperImpl;
+import com.client.calorieserver.domain.dto.request.CreateCalorieRequest;
+import com.client.calorieserver.domain.dto.request.CreateUserCalorieRequest;
+import com.client.calorieserver.domain.mapper.CalorieMapper;
 import com.client.calorieserver.domain.model.Calorie;
 import com.client.calorieserver.utils.TestData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDateTime;
@@ -16,12 +22,16 @@ public class CalorieMapperImplTest {
     private static final LocalDateTime calorie_time = LocalDateTime.now()
             .truncatedTo(ChronoUnit.SECONDS);
     private static final Long default_calorie_id = 1L;
-    private CalorieMapperImpl calorieMapper;
+    private CalorieMapper calorieMapper;
+
+    @Mock
+    CalorieAccessor calorieAccessor;
 
     @BeforeEach
     void init(){
         MockitoAnnotations.openMocks(this);
-        calorieMapper = new CalorieMapperImpl();
+        calorieMapper = Mockito.mock(CalorieMapper.class, Mockito.CALLS_REAL_METHODS);
+        calorieMapper.setCalorieAccessor(calorieAccessor);
     }
 
     @Test
@@ -45,6 +55,38 @@ public class CalorieMapperImplTest {
         calorieMapper.afterMappingCalorie(calorieDTO, calorie);
         assert calorie.getNumCalories().equals(calorieDTO.getNumCalories());
         assert !calorie.isWithinLimit();
+    }
+
+    @Test
+    void afterMappingCalorieCreateCalorieRequest(){
+        CreateCalorieRequest createCalorieRequest = new CreateCalorieRequest();
+        Calorie calorie = TestData.getDefaultCalorie();
+        calorie.setNumCalories(null);
+        ArgumentCaptor<Long> argumentCaptor = ArgumentCaptor.forClass(Long.class);
+        Mockito.when(calorieAccessor.getCalories(Mockito.anyString(), Mockito.anyLong()))
+                .thenReturn(100);
+        calorieMapper.afterMappingCalorie(createCalorieRequest, calorie);
+        Mockito.verify(calorieAccessor).getCalories(Mockito.anyString(), argumentCaptor.capture());
+        assert argumentCaptor.getValue().equals(calorie.getUserId());
+        assert calorie.getNumCalories() == 100;
+        calorieMapper.afterMappingCalorie(createCalorieRequest, calorie);
+        calorie.setNumCalories(100);
+    }
+
+    @Test
+    void afterMappingCalorieUpdateCalorieRequest(){
+        CreateUserCalorieRequest userCalorieRequest = new CreateUserCalorieRequest();
+        Calorie calorie = TestData.getDefaultCalorie();
+        calorie.setNumCalories(null);
+        ArgumentCaptor<Long> argumentCaptor = ArgumentCaptor.forClass(Long.class);
+        Mockito.when(calorieAccessor.getCalories(Mockito.anyString(), Mockito.anyLong()))
+                .thenReturn(100);
+        calorieMapper.afterMappingCalorie(userCalorieRequest, calorie);
+        Mockito.verify(calorieAccessor).getCalories(Mockito.anyString(), argumentCaptor.capture());
+        assert argumentCaptor.getValue().equals(calorie.getUserId());
+        assert calorie.getNumCalories() == 100;
+        calorieMapper.afterMappingCalorie(userCalorieRequest, calorie);
+        calorie.setNumCalories(100);
     }
 
 }
