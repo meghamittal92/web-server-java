5f4f07d4865273532654ad36bd4d5069f5b353ad
Megha Mittal <megha.mittal92@live.com>
2021-02-12 15:30:31 +0530

    Add unique constraint to role name

--- src/main/java/com/client/calorieserver/domain/dto/db/RoleDTO.java
+++ src/main/java/com/client/calorieserver/domain/dto/db/RoleDTO.java
@@ -1,10 +1,8 @@
 package com.client.calorieserver.domain.dto.db;
 
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 import javax.persistence.*;
 import java.util.Collection;
@@ -13,12 +11,13 @@ import java.util.Collection;
 @Table(name = "roles")
 @Getter
 @Setter
-public class RoleDTO extends  AuditableDTO<String> {
+public class RoleDTO extends AuditableDTO<String> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     @ManyToMany(mappedBy = "roleDTOs")
